9d61030f02ec83279b334a4901d691a402538476
Kellie Brownell <kbrownel@stanford.edu>
2017-03-27 12:02:11 -0700

    Add comments and abstract paths

--- delete_modules/delete_modules.sh
+++ delete_modules/delete_modules.sh
@@ -5,42 +5,50 @@ source includes/generate_sites_options.inc
 source includes/remove_module_from_selected_sites.inc
 
 timestamp=$(date +%Y%m%d%H%M%S)
+# archives below this file size will be considered unreliable
 minimum_size=9000
 
-environment="dev"
-sitename_prefix="/Users/kelliebrownell/Sites"
-# Example of a sitename suffix might be public_html on sites1/2 servers
-sitename_suffix=""
+# used for drush alias
+server="uat"
+script_path="~/Sites"
+# absolute path to server's webroot, do trailing slash
+server_webroot="/var/www"
+# example of a sitename suffix might be public_html on sites1/2 servers
+sitename_suffix="public_html"
 
+# which module do you want to delete?
 module_input=$(whiptail --title "Delete a module in sites/default" --inputbox "Which module in sites/default would you like to delete?" 10 60 3>&1 1>&2 2>&3)
 check_exit_status
 
-status_selection=$(whiptail --title "Module Status" --checklist "Only delete sites/default modules with the following status(es).  Note: we do not delete enabled modules." --notags 15 60 3 \
+# limit deletion of module to this status
+status_selection=$(whiptail --title "Module Status" --checklist "Only delete sites/default modules with any (ie. OR) of the following status(es).  Note: we do not delete enabled modules." --notags 15 60 3 \
 not_installed "" on \
 disabled "" off \
 enabled "" off 3>&1 1>&2 2>&3)
 check_exit_status
 
-difference_selection=$(whiptail --title "sites/default Difference" --checklist "Only delete sites/default modules with the following differences from sites/all/modules." --notags 15 60 5 \
+# further limit deletion of module based on a comparison with the same module in sites/all
+difference_selection=$(whiptail --title "sites/default Difference" --checklist "Only delete sites/default modules with any (ie. OR) of the following differences from sites/all/modules." --notags 15 60 5 \
 matches "" off \
 not_in_sites_all "" off \
 default_older "" off \
-default_newer "" off \
 same_version_code_differs "" off 3>&1 1>&2 2>&3)
 check_exit_status
 
+# returns a list of sites with a module in sites/default that matches the selection criteria
 generate_sites_options
+# users can limit the deletion of module to an arbitraty subset of sites meeting the selectoin criteria
 if [ -z "${sites_options[*]}" ]; then
   echo "No sites meet your criteria" && exit
 else
-  sites_selection=$(whiptail --title "Select Sites" --checklist "Only delete the sites/default copy of $module_input from the following sites. Press <space> to make your selection." 25 60 "${#sites_options[@]}" "${sites_options[@]}" --notags 3>&1 1>&2 2>&3)
+  sites_selection=$(whiptail --title "Select Sites" --checklist "Only delete the sites/default copy of $module_input from the following sites. Press <space> to make your selection.  Sites appear on this list if they have met at least one of your status criteria AND at least one of your difference criteria." 25 60 "${#sites_options[@]}" "${sites_options[@]}" --notags 3>&1 1>&2 2>&3)
   check_exit_status
 fi
 
 whiptail --title "Confirmation" --yes-button "PROCEED" --no-button "Cancel"  --yesno "Please confirm that you would like to delete $module_input from ${sites_selection[*]}.  Only if its status is ${status_selection[*]} and difference is ${difference_selection[*]}." 10 60 3>&1 1>&2 2>&3
 check_exit_status
 
-# Double check that the user chose PROCEED
+# double check that the user chose PROCEED
 if [ "$exitstatus" == 0 ]; then
   for site in "${sites_selection[@]}"; do
     # remove quotes
